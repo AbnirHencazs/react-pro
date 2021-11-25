127bf1f5a3a7e657bf889c7da7148794218b04ac
Brian Sánchez <brian.sanchez@inmediatum.com>
2021-11-25 15:37:37 -0600

    refactor CounterBy to use an interface for state

--- src/bases/CounterBy.tsx
+++ src/bases/CounterBy.tsx
@@ -2,19 +2,23 @@ import { useState } from "react"
 interface Props {
     initialValue?: number
 }
+interface CounterState {
+    counter: number;
+    clicks: number;
+}
 export const CounterBy = ({ initialValue = 5 }: Props) => {
-    const [ counter, setCounter ] = useState({
+    const [ { counter, clicks }, setCounter ] = useState<CounterState>({
         counter: initialValue,
         clicks: 0
     })
     return(
         <>
-            <h1>CounterBy: { counter.counter }</h1>
-            <h1>Clicks: {counter.clicks}</h1>
-            <button onClick={ () => setCounter((prev) => ({ clicks: prev.clicks + 1, counter: prev.counter + 1 })) }>
+            <h1>CounterBy: { counter }</h1>
+            <h1>Clicks: { clicks }</h1>
+            <button onClick={ () => setCounter(({ clicks, counter }) => ({ clicks: clicks + 1, counter: counter + 1 })) }>
                 +1
             </button>
-            <button onClick={ () => setCounter((prev) => ({ clicks: prev.clicks + 1, counter: prev.counter + 1 })) }>+5</button>
+            <button onClick={ () => setCounter(({ clicks, counter }) => ({ clicks: clicks + 1, counter: counter + 1 })) }>+5</button>
         </>
     )
 }
