0e80ea6163fe32d55d2bf25025077be41d2cf91f
Brian Sánchez <brian.sanchez@inmediatum.com>
2021-11-25 16:02:42 -0600

    use GreenSock to animate useEffect condition

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { gsap } from 'gsap'
 interface Props {
     initialValue?: number
 }
@@ -8,7 +9,10 @@ const MAXIMUM_VALUE = 10
 export const CounterEffect = ({ initialValue = 0 }: Props) => {
     const [ counter, setCounter ] = useState(initialValue)
     useEffect(() => {
-
+        if(counter < MAXIMUM_VALUE) return
+        gsap.to('h2', { y: -10, duration: 0.2, ease:'ease.out' }).then(() => {
+            gsap.to('h2', { y:0, duration: 1, ease: 'bounce.out'})
+        })
     }, [counter])
     const handleClick = () => {
         if(counter < MAXIMUM_VALUE){
@@ -17,7 +21,8 @@ export const CounterEffect = ({ initialValue = 0 }: Props) => {
     }
     return(
         <>
-            <h1>Counter effect: { counter }</h1>
+            <h1>Counter effect:</h1>
+            <h2>{counter}</h2>
             <button onClick={ handleClick }>+1</button>
         </>
     )
