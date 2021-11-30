3b9ecc3ffea721b25ad373817dc5a66e870d4041
Brian Sánchez <briansanchezizq@gmail.com>
2021-11-30 14:58:35 -0600

    create custom hook useCounter

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react"
+import { gsap } from "gsap"
+
+const MAXIMUM_VALUE = 10
+
+export const useCounter = () => {
+    const [ counter, setCounter ] = useState(5);
+    const counterElement = useRef<HTMLHeadingElement>(null);
+
+    const handleClick = () => {
+        if(counter < MAXIMUM_VALUE){
+            setCounter((prev) => prev + 1)
+        }
+    }
+
+    useEffect(() => {
+        if(counter < MAXIMUM_VALUE) return
+
+        const timeLine = gsap.timeline()
+        timeLine.to( counterElement.current, { y: -10, duration: 0.2, ease:'ease.out' } )
+        .to( counterElement.current, { y:0, duration: 1, ease: 'bounce.out'} )
+
+    }, [counter])
+    
+    return{
+        counter,
+        counterElement,
+        handleClick
+    }
+}
