5658227c031ae02586f97b89df91a44ceb5363c7
Brian Sánchez <briansanchezizq@gmail.com>
2021-11-30 15:13:39 -0600

    pass params to useCounter and refactor custom hook in order to use a ref on green sock object

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,30 +1,31 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 import { gsap } from "gsap"
 
-const MAXIMUM_VALUE = 10
-
-export const useCounter = () => {
+export const useCounter = ({ maxCount = 10 }) => {
     const [ counter, setCounter ] = useState(5);
-    const counterElement = useRef(null);
+    const elementToAnimate = useRef(null);
+    const timeLine = useRef( gsap.timeline() )
 
     const handleClick = () => {
-        if(counter < MAXIMUM_VALUE){
+        if(counter < maxCount){
             setCounter((prev) => prev + 1)
         }
     }
+    useLayoutEffect(() => {
+        if( !elementToAnimate.current ) return
+        timeLine.current
+            .to( elementToAnimate.current, { y: -10, duration: 0.2, ease:'ease.out' } )
+            .to( elementToAnimate.current, { y:0, duration: 1, ease: 'bounce.out'} )
+            .pause();
+    }, [])
 
     useEffect(() => {
-        if(counter < MAXIMUM_VALUE) return
-
-        const timeLine = gsap.timeline()
-        timeLine.to( counterElement.current, { y: -10, duration: 0.2, ease:'ease.out' } )
-        .to( counterElement.current, { y:0, duration: 1, ease: 'bounce.out'} )
-
+        timeLine.current.play(0)
     }, [counter])
     
     return{
         counter,
-        counterElement,
+        elementToAnimate,
         handleClick
     }
 }
