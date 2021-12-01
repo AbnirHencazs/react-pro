130f779e3f55ba13f63f820084a0908fd5660247
Brian Sánchez <briansanchezizq@gmail.com>
2021-12-01 13:30:05 -0600

    implement a useReducer

---
+++ src/bases/CounterReducer.tsx
@@ -0,0 +1,41 @@
+import { useReducer, useState } from "react"
+
+interface CounterState {
+    counter: number;
+    previous: number;
+    changes: number;
+}
+const INITIAL_STATE: CounterState = {
+    counter: 10,
+    previous: 15,
+    changes: 20
+}
+
+type CounterAction = 
+    | { type: 'increaseBy', payload: { value: number } }
+    | { type: 'reset' }
+
+const counterReducer = (state: CounterState, action: CounterAction): CounterState => {
+    switch (action.type) {
+        case 'reset':
+            return{
+                changes: 0,
+                counter: 0,
+                previous: 0
+            }
+        default:
+            return state;
+    }
+}
+
+export const CounterReducerComponent = () => {
+    const [ { counter }, dispatch ] = useReducer( counterReducer, INITIAL_STATE )
+    return(
+        <>
+            <h1>Counter Reducer: { counter }</h1>
+            <button onClick={ () => dispatch({ type: 'reset'}) }>
+                Reset
+            </button>
+        </>
+    )
+}
